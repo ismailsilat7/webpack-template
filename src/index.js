215a888ae222d372a21c5bcff7315244a81f0eb1
Ismail Silat <ismailsilat7@gmail.com>
2025-07-29 09:21:39 +0500

    Made prettier by prettier :)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './style.css';
-import logo from './images/logo.png';
+import "./style.css";
+import logo from "./images/logo.png";
 
-document.getElementById('app').innerText = 'by Ismail :)';
+document.getElementById("app").innerText = "by Ismail :)";
 
-const img = document.createElement('img');
-img.classList.add('logo')
+const img = document.createElement("img");
+img.classList.add("logo");
 img.src = logo;
 document.body.appendChild(img);
